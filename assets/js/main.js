433ebdc7461cf4276da179ea1683d4ce281af804
raccooog <yer111an0@gmail.com>
2026-06-24 03:25:10 +0500

    готово

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
 console.log("Kloré Skin Loaded");
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+const button = item.querySelector(".faq-question");
+
+button.addEventListener("click", () => {
+
+item.classList.toggle("active");
+
+});
+
+});
